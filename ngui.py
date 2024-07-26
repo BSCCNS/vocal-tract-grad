c2d2608aa78d5593eaf291da2afaa62f693a1496
Jofre Vallès <jofrevalles99@gmail.com>
2024-07-26 15:34:45 +0200

    Add files for py2app creation

--- ngui.py
+++ ngui.py
@@ -12,7 +12,7 @@ def handle_upload(e):
 
 ui.label('Voice resynthesis with GFM')
 ui.separator()
-with ui.row().classes('w-full no-wrap'):    
+with ui.row().classes('w-full no-wrap'):
     # left panel
     with ui.column().classes('w-2/6'): # buttons
         with ui.row():
@@ -25,49 +25,49 @@ with ui.row().classes('w-full no-wrap'):
         ui.separator()
         with ui.row():
             ui.label("Load audio:")
-            ui.upload(on_upload=handle_upload).props('accept=.wav').classes('max-w-full')            
+            ui.upload(on_upload=handle_upload).props('accept=.wav').classes('max-w-full')
             ui.button('Play audio', on_click=driver.play_audio, color='grey')
 
-        ui.separator() 
+        ui.separator()
         ui.label("Select input")
         select_input = ui.select(driver.inputs, value=driver.cur_input, on_change=driver.update_selected_input)
         ui.label("Select output")
         select_output = ui.select(driver.outputs, value=driver.cur_output, on_change=driver.update_selected_output)
         ui.button('Refresh devices', on_click=driver.refresh_devices, color='grey')
 
-    with ui.column().classes('w-2/6'): # sliders 
+    with ui.column().classes('w-2/6'): # sliders
 # mover F2 entre F1 y F3 (al subir lengua adelante)
 # mover F3 entre F2 y F4 (al bajar cierra labios)
-        F1_slider = ui.slider(min=-100, max=100, 
-                              step=1, value=0).on('update:model-value', 
+        F1_slider = ui.slider(min=-100, max=100,
+                              step=1, value=0).on('update:model-value',
                                 lambda e: driver.update_F1(e.args),throttle=1.0)
-        ui.label().bind_text_from(F1_slider, 'value', 
+        ui.label().bind_text_from(F1_slider, 'value',
                                   backward=lambda n: f'<-- close jaw        F1 %:: {n}          open jaw -->')
         # ui.label().bind_text_from(F1_slider, 'value')
         F2_slider = ui.slider(min=-100, max=100, step=1, value=0) \
             .on('update:model-value', lambda e: driver.update_F2(e.args),
             throttle=1.0)
-        ui.label().bind_text_from(F2_slider, 'value', 
+        ui.label().bind_text_from(F2_slider, 'value',
                                   backward=lambda n: f'<-- tongue forward      F2%::  {n}      tongue backwards')
         # ui.label().bind_text_from(F1_slider, 'value')
         F3_slider = ui.slider(min=-100, max=100, step=1, value=0) \
             .on('update:model-value', lambda e: driver.update_F3(e.args),
             throttle=1.0)
-        ui.label().bind_text_from(F3_slider, 'value', 
+        ui.label().bind_text_from(F3_slider, 'value',
                                   backward=lambda n: f'<-- close lips         F3%:: {n}       open lips -->')
-        
-        # ui.label().bind_text_from(F1_slider, 'value')        
+
+        # ui.label().bind_text_from(F1_slider, 'value')
         t_switch = ui.switch('Activate tenseness')
         with ui.column().bind_visibility_from(t_switch, 'value'):
             tenseness_slider = ui.slider(min=-100, max=100, step=1, value=0) \
                 .on('update:model-value', lambda e: driver.update_tenseness(e.args),
                 throttle=1.0)
-            ui.label().bind_text_from(tenseness_slider, 'value', 
+            ui.label().bind_text_from(tenseness_slider, 'value',
                                   backward=lambda n: f'Tenseness stretch (-100 to 100):: {n}')
             vocalness_slider = ui.slider(min=-100, max=100, step=1, value=0) \
                 .on('update:model-value', lambda e: driver.update_tenseness(e.args),
                 throttle=1.0)
-            ui.label().bind_text_from(vocalness_slider, 'value', 
+            ui.label().bind_text_from(vocalness_slider, 'value',
                                   backward=lambda n: f'Vocalness stretch (-100 to 100):: {n}')
     with ui.column().classes('bg-blue-100 w-2/6'): # knobs
         measure = ui.button('Latency', on_click=lambda: ui.notify(f'{int(driver.get_latency()*100)} ms'), color='grey')
